40090981b0dc61627d36abd95cc04319ad1b730b
Bogdan Matasaru <bogdan.matasaru@emag.ro>
2018-09-08 15:02:36 +0300

    initial commit

---
+++ eMOVE/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  eMOVE
+//
+//  Created by Bogdan Matasaru on 08/09/2018.
+//  Copyright © 2018 Bogdan Matasaru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
